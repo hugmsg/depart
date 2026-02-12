88ffff34dd8f3154b82fd9f5c6ae4bea5e202395
hugmsg <97217173+hugmsg@users.noreply.github.com>
2026-02-12 16:22:05 +0100

    Refactor service worker for new cache name and URLs

--- sw.js
+++ sw.js
@@ -1,42 +1,19 @@
-const CACHE_NAME = "scanqr-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.json",
-  "https://unpkg.com/html5-qrcode"
+const CACHE_NAME = "depart-pwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/manifest.json"
 ];
 
-// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// ACTIVATE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k !== CACHE_NAME)
-          .map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
-
